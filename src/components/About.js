058844c35fb8db291d7734a18983d100ae357d80
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-06-12 10:45:32 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,13 @@ export default function About(props) {
   return (
     <>
     <div>
-        <div className="card container " style={{width: '470px', marginBottom:'100px',backgroundColor:props.mode==='light'? 'white' :'black',color:props.mode==='light'? 'black' :'white', border:'solid 1px grey'}}>
-        <img src="/image.png" className="card-img-top"   alt="..."/>
-        <div className="card-body">
-            <p className="card-text" >"TextUtils is an intuitive online tool crafted to streamline a range of text-related tasks. Whether you need to convert text to uppercase, lowercase, or capitalize sentences, TextUtils has you covered. This tool aims to make your text manipulation tasks easier and more efficient. With a user-friendly interface, TextUtils is suitable for writers, students, and professionals seeking quick solutions for their text processing needs."</p>
+        <div className="container p-5" style={{ marginBottom:'100px',backgroundColor:props.mode==='light'? 'white' :'black',color:props.mode==='light'? 'black' :'white', animation:"float 5s ease-in-out"}}>
+       
+       <h3 className="primary-color">About TextUtils</h3>
+        <div>
+            <p  className='py-4'>"TextUtils is an intuitive online tool crafted to streamline a range of text-related tasks. <br />Whether you need to convert text to uppercase, lowercase, or capitalize sentences, TextUtils has you covered. This tool aims to make your text manipulation tasks easier and more efficient. <br />
+            With a user-friendly interface, TextUtils is suitable for writers, students, and professionals seeking quick solutions for their text processing needs. <br /><br />Whether you are drafting an important document or preparing a presentation, TextUtils simplifies the process, allowing you to focus on your content rather than formatting. <br />
+            Experience the convenience and efficiency of TextUtils today, and discover how it can transform your approach to text processing and manipulation."</p>
         </div>
         </div>
     </div>
